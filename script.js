92f3a8b0b1f0ecbb8823def527332a581b52ee26
Barshinger Kids <barshingerkids@Davids-iMac.local>
2020-07-14 15:02:18 -0500

    finish project page design

--- script.js
+++ script.js
@@ -5,28 +5,41 @@
 const contentcontainer = document.querySelector('#content');
 let contentOpen = false;
 
-function newContent(title, iconSrc, description) {
-    if (contentOpen = false) {
-        document.querySelectorAll('.content')[0].remove();
-    } else {
-        contentOpen = true;
-    }
+function newProjectContent(title, imageSrc, description, website, github) {
+        contentcontainer.innerHTML = "";
+        let content = contentcontainer;
+        let image = `<img src="${imageSrc}">`;
+        content.innerHTML += image;
+        content.innerHTML += `<h2>${title}</h2>`;
+        content.innerHTML += `<p>${description}</p>`;
+        content.innerHTML += `<p>Website: <a href="${website}">${website}</a></p>`;
+        content.innerHTML += `<p>GitHub: <a href="${github}">${github}</a></p>`;
+        content.classList += "content";
+}
+
+function newAboutContent(title, imageSrc, description, website, github) {
+    contentcontainer.innerHTML = "";
     let content = contentcontainer;
-    let icon = `<img src="${iconSrc}">`;
-    content.innerHTML += icon;
+    let image = `<img src="${imageSrc}">`;
+    content.innerHTML += image;
     content.innerHTML += `<h2>${title}</h2>`;
     content.innerHTML += `<p>${description}</p>`;
+    content.innerHTML += `<p>Website: <a href="${website}">${website}</a></p>`;
+    content.innerHTML += `<p>GitHub: <a href="${github}">${github}</a></p>`;
     content.classList += "content";
-  }
+}
 
 let blobsNodeList = document.querySelectorAll('.blob');
 let blobsArray = Array.from(blobsNodeList);
 
 blobsArray.forEach(function(blob, index) {
+    if (blob.classList.contains(project))
     blob.addEventListener('click', () => {
         let contentTitle = blob.getAttribute('data-title');
-        let contentIconSrc = blob.getAttribute('data-icon-src');
+        let contentImageSrc = blob.getAttribute('data-image-src');
         let contentDescription = blob.getAttribute('data-description');
-        newContent(blob.getAttribute('data-title'), blob.getAttribute('data-icon-src'), blob.getAttribute('data-description'))
+        let contentWebsite = blob.getAttribute('data-website');
+        let contentGithub = blob.getAttribute('data-github');
+        newProjectContent(contentTitle, contentImageSrc, contentDescription, contentWebsite, contentGithub)
     })
 })
