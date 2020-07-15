ab2c60ea2ba55e9dd38f84765aad345d23f9dd66
Barshinger Kids <barshingerkids@Davids-iMac.local>
2020-07-15 11:04:59 -0500

    comment on js code

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+/* VARIABLES */
+
+// Gets a node list of all blobs in html.
 const blobsNodeList = document.querySelectorAll('.blob');
+// Turns blob node list into array.
 const blobsArray = Array.from(blobsNodeList);
+// Gets a list of all content divs in html
 const contentNodeList = document.querySelectorAll('.content');
+// Turns content node list into array.
 const contentArray = Array.from(contentNodeList);
 
+/* MAIN */
+
+// Loops through blobs array
 blobsArray.forEach(function(blob, index) {
+    // Adds listener for clicks on each blob
     blob.addEventListener('click', () => {
+        // On click, loops through content array
         contentArray.forEach(function(content, index) {
+            // Finds all non-hidden items
             if (content.classList.contains("hidden") === false) {
+                // Hides previously non-hidden items
                 content.classList.add("hidden");
             }
         })
+        // Unhides correct item
         contentArray[index].classList.toggle("hidden");
     })
 })
